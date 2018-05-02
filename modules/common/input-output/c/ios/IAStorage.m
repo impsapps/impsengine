b1a08f39a1234d6352c730bbd60ef83fcab585ef
Konstantin Merker <konstantin.merker@impsapps.com>
2018-05-02 09:58:48 +0200

    changed private data dir on ios

--- modules/common/input-output/c/ios/IAStorage.m
+++ modules/common/input-output/c/ios/IAStorage.m
@@ -10,7 +10,8 @@
 #include "IAStorage+Native.h"
 
 void IAStorage_nativeGetPrivateDataDir(IAString * privateDataDir){
-    NSString * filePath = [NSString stringWithFormat:@"%@/Library/Application Support", NSHomeDirectory()];
+    NSString * identifier = [[[NSBundle mainBundle] infoDictionary] objectForKey:(NSString*)kCFBundleIdentifierKey];
+    NSString * filePath = [NSString stringWithFormat:@"%@/Library/Application Support/%@", NSHomeDirectory(), identifier];
     const char * str = [filePath UTF8String];
     IAString_set(privateDataDir, str);
 }
