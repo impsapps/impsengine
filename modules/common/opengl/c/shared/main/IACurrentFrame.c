1bfb8ab537905638a876035a7a43a2d35c93f5bd
Konstantin Merker <konstantin.merker@impsapps.com>
2018-05-31 19:14:54 +0200

    implemented IACurrentFrame_getDeltaTimeSinceLastFrame

--- modules/common/opengl/c/shared/main/IACurrentFrame.c
+++ modules/common/opengl/c/shared/main/IACurrentFrame.c
@@ -6,16 +6,28 @@
 //  Copyright (c) 2015 Konstantin Merker, Paul Wallrabe und Martin Krautschick GbR (Imps Apps). All rights reserved.
 //
 
+#include "IALibrary.h"
 #include "IACurrentFrame.h"
 #include "IACurrentFrame+Private.h"
 
 static uint64_t currentTimeForFrame = 0;
+static uint64_t deltaTimeSinceLastFrame = 0;
 
-void IACurrentFrame_setTime(uint64_t currentTimeForFrameTemp){
-    currentTimeForFrame = currentTimeForFrameTemp;
+void IACurrentFrame_setTime(uint64_t newTime){
+    debugAssert(newTime >= currentTimeForFrame && "Time must be monotonic increasing");
+    static bool wasSet = false;
+    if (wasSet == true) {
+        deltaTimeSinceLastFrame = newTime - currentTimeForFrame;
+    }
+    currentTimeForFrame = newTime;
+    wasSet = true;
 }
 
 uint64_t IACurrentFrame_getTime(){
     return currentTimeForFrame;
+}
+
+uint64_t IACurrentFrame_getDeltaTimeSinceLastFrame(){
+    return deltaTimeSinceLastFrame;
 }
 
